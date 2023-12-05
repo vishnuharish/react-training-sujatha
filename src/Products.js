8e8ac74c52e3ee5ec7c27e902bac532c4d032b55
Harish <vishnuharish12@gmail.com>
2023-12-05 21:58:07 +0530

    Update Products.js

--- src/Products.js
+++ src/Products.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 
-export default class Products extends Component {
+export class Products extends Component {
     constructor(props){
         super(props)
         this.state = {
